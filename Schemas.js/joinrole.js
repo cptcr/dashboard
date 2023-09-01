59dfbf5fc8e05771ef9409a5aa548d17e4fa7c11
toowake <109671426+toowake@users.noreply.github.com>
2023-09-01 18:15:21 +0200

    Delete Schemas.js/joinrole.js

--- Schemas.js/joinrole.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-let joinrole = new Schema({
-    Guild: String,
-    RoleID: String,
-    RoleName: String
-})
-
-module.exports = model("joinrole", joinrole);
