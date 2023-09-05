946b7a11b460b3ac5cf7d3fd57bf2fc08155c6b9
toowake <109671426+toowake@users.noreply.github.com>
2023-09-05 19:09:52 +0200

    Add files via upload

---
+++ Locales/Languages/czech.js
@@ -0,0 +1,137 @@
+const locales = {
+    name: "Czech",
+    index: {
+        feeds: ["Současní uživatelé", "Procesor", "Systémová platforma", "Počet Serverů"],
+        card: {
+            image: "https://cdn.discordapp.com/attachments/1121354024222998630/1147492191585972274/image.png",
+            category: "Nexus",
+            title: "NEXUS - představte si bezplatného bota Discord",
+            description:
+                "Objevte dokonalé řešení Discord bota bez nutnosti drahých prémiových služeb - představujeme vám našeho funkcemi nabitého bota zdarma! Sestavili jsme komplexní sadu nástrojů, které zlepší zážitek z vašeho serveru a učiní ho pro vaši komunitu zajímavějším.",
+            footer: "",
+        },
+        feedsTitle: "Feeds",
+        graphTitle: "Graphs"
+    },
+    manage: {
+        settings: {
+            memberCount: "Členové",
+            info: {
+                info: "Info",
+                server: "Informace o serveru"
+            }
+        },
+        title: "Tvoje servery",
+        description: "Správa serverů",
+    },
+    admin: {
+        feeds: {
+            feedBuilder: "Feed Stavitel",
+            feedIcon: "Feed Ikona",
+            feedDescription: "Feed Popisek",
+            feedColour: "Feed Barva",
+            colors: {
+                pink: "Růžová",
+                red: "Červená",
+                orange: "Oranžová",
+                green: "Zelená",
+                gray: "Šedá",
+                blue: "Modrá",
+                dark: "Tmavá",
+            },
+            feedSubmit: "Odeslat",
+            feedFeedPreview: "Náhled",
+            feedPreview: "Náhled",
+            feedCurrent: "Feeds",
+            feedShowIcons: "Ikony",
+        },
+        admin: {
+            title: "Ovládací prvky správce",
+            adminUpdates: "Kontrola aktualizací",
+        },
+    },
+    guild: {
+        home: "Domov",
+        settingsCategory: "Nastavení",
+        updates: {
+            title: "Viditelné změny!",
+            reset: "Obnovení",
+            save: "Uložit",
+        }
+    },
+    privacyPolicy: {
+        title: "Zásady ochrany osobních údajů",
+        description: "Zásady ochrany osobních údajů a Podmínky služby",
+        pp: "Kompletní zásady ochrany osobních údajů"
+    },
+    partials: {
+        sidebar: {
+            dash: "Přístrojová deska",
+            manage: "Správa serverů",
+            commands: "Příkazy",
+            pp: "Zásady ochrany osobních údajů",
+            admin: "Administrátor",
+            account: "Stránky účtu",
+            login: "Přihlásit se",
+            logout: "Odhlásit se"
+        },
+        navbar: {
+            home: "Domov",
+            pages: {
+                manage: "Správa serverů",
+                settings: "Správa serverů",
+                commands: "Příkazy",
+                pp: "Zásady ochrany osobních údajů",
+                admin: "Administrátorský Panel",
+                error: "Error",
+                credits: "Kredity",
+                debug: "Ladění",
+                leaderboard: "Žebříček",
+                profile: "Profil",
+                maintenance: "Údržba",
+                pages: "Stránky",
+                dashboard: "Nastavení",
+            }
+        },
+        title: {
+            pages: {
+                manage: "Správa serverů",
+                settings: "Správa serverů",
+                commands: "Příkazy",
+                pp: "Zásady ochrany osobních údajů",
+                admin: "Administrátor",
+                error: "Error",
+                credits: "Kredity",
+                debug: "Ladění",
+                leaderboard: "Žebříček",
+                profile: "Profil",
+                maintenance: "Údržba"
+            }
+        },
+        preloader: {
+            text: "Načítání..."
+        },
+        premium: {
+            title: "Chceš Premium?",
+            description: "Lol my nevíme, co je to premium",
+            buttonText: "podpoř naši práci"
+        },
+        settings: {
+            title: "Konfigurace webu",
+            description: "Konfigurovatelné možnosti zobrazení",
+            theme: {
+                title: "Témata webu",
+                description: "Přizpůsob si stránky přitažlivější pro svoje oči!",
+                dark: "Tmavý",
+                light: "Světlý",
+                auto: "Automatický"
+            },
+            language: {
+                title: "Jazyk stránky",
+                description: "Vyber jazyk"
+            }
+        }
+    }
+};
+
+module.exports = locales;
---
+++ Locales/Languages/simple-chinese.js
@@ -0,0 +1,137 @@
+const locales = {
+        name: "Chinese",
+        index: {
+            feeds: ["当前用户", "CPU", "系统平台", "服务器数量"],
+            card: {
+                image: "https://cdn.discordapp.com/attachments/1121354024222998630/1147492191585972274/image.png",
+                category: "尼克斯",
+                title: "NEXUS - 想象一个免费的Discord机器人",
+                description:
+                    "发现终极Discord机器人解决方案，无需昂贵的高级服务 - 我们呈现了我们功能丰富的免费机器人！我们汇集了一套全面的工具，以增强您的服务器体验，使其对您的社区更具吸引力。",
+                footer: "",
+            },
+            feedsTitle: "动态",
+            graphTitle: "图表"
+        },
+        manage: {
+            settings: {
+                memberCount: "成员",
+                info: {
+                    info: "信息",
+                    server: "服务器信息"
+                }
+            },
+            title: "您的服务器",
+            description: "管理您的公会",
+        },
+        admin: {
+            feeds: {
+                feedBuilder: "动态构建器",
+                feedIcon: "动态图标",
+                feedDescription: "动态描述",
+                feedColour: "动态颜色",
+                colors: {
+                    pink: "粉色",
+                    red: "红色",
+                    orange: "橙色",
+                    green: "绿色",
+                    gray: "灰色",
+                    blue: "蓝色",
+                    dark: "深色",
+                },
+                feedSubmit: "提交",
+                feedFeedPreview: "预览",
+                feedPreview: "预览",
+                feedCurrent: "动态",
+                feedShowIcons: "图标",
+            },
+            admin: {
+                title: "管理员控制",
+                adminUpdates: "检查更新",
+            },
+        },
+        guild: {
+            home: "首页",
+            settingsCategory: "设置",
+            updates: {
+                title: "看到的更改！",
+                reset: "重置",
+                save: "保存",
+            }
+        },
+        privacyPolicy: {
+            title: "隐私政策",
+            description: "隐私政策和服务条款",
+            pp: "完整的隐私政策"
+        },
+        partials: {
+            sidebar: {
+                dash: "仪表板",
+                manage: "管理公会",
+                commands: "命令",
+                pp: "隐私政策",
+                admin: "管理员",
+                account: "帐户页面",
+                login: "登录",
+                logout: "登出"
+            },
+            navbar: {
+                home: "首页",
+                pages: {
+                    manage: "管理公会",
+                    settings: "管理公会",
+                    commands: "命令",
+                    pp: "隐私政策",
+                    admin: "管理员面板",
+                    error: "错误",
+                    credits: "积分",
+                    debug: "调试",
+                    leaderboard: "排行榜",
+                    profile: "个人资料",
+                    maintenance: "维护中",
+                    pages: "页面",
+                    dashboard: "设置",
+                }
+            },
+            title: {
+                pages: {
+                    manage: "管理公会",
+                    settings: "管理公会",
+                    commands: "命令",
+                    pp: "隐私政策",
+                    admin: "管理员",
+                    error: "错误",
+                    credits: "积分",
+                    debug: "调试",
+                    leaderboard: "排行榜",
+                    profile: "个人资料",
+                    maintenance: "维护中"
+                }
+            },
+            preloader: {
+                text: "加载中..."
+            },
+            premium: {
+                title: "想要高级？",
+                description: "哈哈，我们不知道什么是高级",
+                buttonText: "支持我们的工作"
+            },
+            settings: {
+                title: "网站配置",
+                description: "可配置的查看选项",
+                theme: {
+                    title: "网站主题",
+                    description: "使网站更吸引人",
+                    dark: "深色",
+                    light: "浅色",
+                    auto: "自动"
+                },
+                language: {
+                    title: "网站语言",
+                    description: "选择您的语言"
+                }
+            }
+        }
+}
+    
+module.exports = locales;
