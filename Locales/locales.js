8e8455f9a7cda3f69d4cc8be70be8d9275ee9ca0
toowake <109671426+toowake@users.noreply.github.com>
2023-09-05 19:10:24 +0200

    Add files via upload

--- Locales/locales.js
+++ Locales/locales.js
@@ -2,13 +2,19 @@
 //German by toowake
 //Dutch by jasondev
 //Russian by franki1902.
+//Chinese by Chat GPT
+//czech by dev.diortecxcz
 const enUS = require("./Languages/english-us");
 const deDE = require("./Languages/german");
 const nlNL = require("./Languages/dutch");
+const csCZ = require("./Languages/czech");
+const zhCN = require("./Languages/simple-chinese");
 const locales = {
     enUS: enUS ,
     deDE: deDE,    
     nlNL: nlNL,
+    csCZ: csCZ,
+    zhCN: zhCN,
 };
 
 module.exports = locales
