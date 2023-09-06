1c79524b3723b1b365938bc8549a0144484b565b
toowake <109671426+toowake@users.noreply.github.com>
2023-09-06 17:18:13 +0200

    Update locales.js

--- Locales/locales.js
+++ Locales/locales.js
@@ -3,18 +3,31 @@
 //Dutch by jasondev
 //Russian by franki1902.
 //Chinese by Chat GPT
+//Hungarian by Baluuh
 //czech by dev.diortecxcz
 const enUS = require("./Languages/english-us");
 const deDE = require("./Languages/german");
 const nlNL = require("./Languages/dutch");
 const csCZ = require("./Languages/czech");
 const zhCN = require("./Languages/simple-chinese");
+const huHU = require("./Languages/hungarian");
+const trTR = require("./Languages/turkish");
+const frFR = require("./Languages/french");
+const ruRU = require("./Languages/russian");
+const esES = require("./Languages/spanish");
+
+//export
 const locales = {
     enUS: enUS ,
     deDE: deDE,    
     nlNL: nlNL,
     csCZ: csCZ,
     zhCN: zhCN,
+    huHU: huHU,
+    trTR: trTR,
+    frFR: frFR,
+    ruRU: ruRU,
+    esES: esES
 };
 
 module.exports = locales
